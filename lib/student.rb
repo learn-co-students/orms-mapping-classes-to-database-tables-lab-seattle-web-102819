6145476ade190df6a902a508f5d80f028c2050a4
4myc <missch3n@gmail.com>
2019-11-10 22:55:44 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -6,28 +6,21 @@ class Student
   attr_reader :id
   attr_accessor :name, :grade
 
-  @@all = []
-
   def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
     @id = id
-    @@all << self
   end
 
-  def self.all
-    @@all
-  end
-  
   def self.create_table
     sql =  <<-SQL 
-		      CREATE TABLE IF NOT EXISTS students (
-		        id INTEGER PRIMARY KEY, 
-		        name TEXT, 
-		        grade INTEGER
-		        )
-		        SQL
-		    DB[:conn].execute(sql) 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql) 
   end
 
   def self.drop_table
